eaeb6f6bc9da10db762e1cbddcf6673f8403d011
heymitali <mitalisinghnaruka@gmail.com>
2024-04-27 00:57:41 +0530

    fixed all stylings

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,15 +2,15 @@ const Header = () => {
   return (
     <div>
       <div className="mx-20 my-6 flex justify-between">
-        <div className="w-[123px] h-full lg:w-48 lg:h-9">
+        <div className=" w-[191px] h-[39px]">
           <img src="https://res.cloudinary.com/do4vytmxm/image/upload/v1695803892/images/Logo_ev7w4u.png" />
         </div>
-        <div className="">
-          <span className="mx-4 my-2">Home</span>
-          <span className="mx-4 my-2">Flashcard</span>
-          <span className="mx-4 my-2">Contact</span>
-          <span className="mx-4 my-2">FAQ</span>
-          <button className=" mx-4 my-2 rounded-3xl px-4 py-1 text-white bg-[#05286e] w-28">
+        <div className="font-light text-[#3A3740] text-[18px]">
+          <span className="mx-4 my-2 cursor-pointer">Home</span>
+          <span className="mx-4 my-2 cursor-pointer">Flashcard</span>
+          <span className="mx-4 my-2 cursor-pointer">Contact</span>
+          <span className="mx-4 my-2 cursor-pointer">FAQ</span>
+          <button className="w-[128px] h-[48px] mx-4 my-2 rounded-3xl px-[40px] py-[13px] text-white bg-gradient-to-b from-[#06286E] to-[#164EC0]">
             Login
           </button>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "gradient-linear":
+          "linear-gradient(222.94deg, #051A91 -4.31%, #061C93 14.41%, #2284F1 81.88%, #1F80EB 103.81%)",
+      },
+    },
   },
   plugins: [],
 };
